b88c3838f7e5b2494ffd6ef9298f81f9c8cfe0bf
Tristan MARIE <tristan.marie@geodis.com>
2018-09-18 15:08:20 +0200

    Ajout des services Camunda

--- BPM/src/main/java/kt/bpm/controllers/MeteoController.java
+++ BPM/src/main/java/kt/bpm/controllers/MeteoController.java
@@ -2,15 +2,20 @@ package kt.bpm.controllers;
 
 import kt.bpm.models.MeteoAnswer;
 import kt.bpm.models.MeteoMessage;
+import org.camunda.bpm.client.ExternalTaskClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MeteoController {
+    @Autowired
+    private ExternalTaskClient client;
+
     @MessageMapping("/meteo")
-    @SendTo("/topic/meteo")
-    public MeteoAnswer meteo(MeteoMessage message) {
-        return new MeteoAnswer("Il fera beau");
+    public void meteo(MeteoMessage message) {
+        // Démarrer le process
+
     }
 }
